ee00b11a83d17d4a6eca410b155663294ae9c9f6
ghonosham <ghonoshamroy61@gmail.com>
2024-02-22 17:34:13 +0600

    added project without javascript

---
+++ dom.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+function getConvertedValue(total-seat){
+    const seat = document.getElementById(total-seat).innerText;
+    const convertSeat = parseInt{seat};
+    return convertSeat;
+}
+const result = getConvertedValue("budget")
+console.log(result)
